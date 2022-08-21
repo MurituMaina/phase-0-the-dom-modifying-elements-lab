1b07dcff1055ad2c5ff7d9082f5f33bd5987b50b
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-21 14:43:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your code here!
 document.querySelector("main").remove();
 
-
+let newHeader =document.createElement("h1");
+newHeader.setAttribute('id','victory')
+newHeader.textContent ="YOUR-NAME is the champion";
+document.getElementsByTagName("body")
