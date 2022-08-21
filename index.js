2026d22a06e54552a5506c6b4fc44191c8eb0c5e
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-21 14:42:16 +0300

    modified assignment

--- index.js
+++ index.js
@@ -2,7 +2,3 @@
 document.querySelector("main").remove();
 
 
-let newHeader =document.createElement("h1");
-newHeader.setAttribute('id','victory')
-newHeader.textContent ="YOUR-NAME is the champion";
-document.getElementsByTagName("body")
